95a0b7e6f443ef6df746e725d3a9c83efe076c69
David Yim <yimd85@gmail.com>
2017-11-21 20:43:52 -0500

    added recursion another instance

--- my-cool-app/src/App.js
+++ my-cool-app/src/App.js
@@ -18,28 +18,16 @@ class App extends Component {
   render() {
 
     const heroes = this.state.response;
-    console.log(heroes);
-
+    // console.log(heroes);
 
     return (
       <div>
-        <input
-          placeholder='search'
-          onChange={(event) => this.handleChange(event)}
-        />
-        <button onClick={() => this.handleClick()}>
-          go
-        </button>
         <h3>
           {this.state.searchText}
         </h3>
         <p>
           hero: { heroes.length > 0 ? heroes[0].name : ""}
         </p>
-        <p>
-          bio: { heroes.length > 0 ? heroes[0].description : ""}
-        </p>
-
         <img
           src = { heroes.length > 0 ? heroes[0].thumbnail.path+'.'+heroes[0].thumbnail.extension : ""}
           width="50%"
@@ -57,13 +45,13 @@ class App extends Component {
     let publickey = "3ebfa70440b616a86216298f97fa42ed";
     let privatekey = "6ce518b1fc2eab73750129ec04048158fb930961";
     let hash = md5(ts+privatekey+publickey);
-    // console.log(hash);
-    // console.log(ts);
-
+    let min = 0;
+    let max = 1490;
+    let random = Math.floor(Math.random() * (max - min + 1)) + min;
     let configuration = {
       params: {
-        offset: 300,
-        // name:'hulk',
+        offset: random,
+        limit: 1,
         ts: ts,
         apikey: publickey,
         hash: hash
@@ -73,13 +61,21 @@ class App extends Component {
     axios
 
       .get('https://gateway.marvel.com:443/v1/public/characters',configuration)
-
-      // .get('http://www.omdbapi.com/', configuration)
       .then((res) => {
-        console.log(res.data.data.results);
-        this.setState({
-          response: res.data.data.results
-        });
+        // console.log(res.data.data.results);
+        // console.log(res.data.data.results[0].thumbnail);
+        if(res.data.data.results[0].thumbnail.path !== "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available"){
+
+          this.setState({
+            response: res.data.data.results
+
+          });
+      }
+
+      else{
+        console.log('worked');
+        this.componentDidMount();
+      }
       });
 
   }
--- my-cool-app/src/index.js
+++ my-cool-app/src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+      <div>
+      		<App />
+      		<App />
+      </div>
+  ,document.getElementById('root'));
 registerServiceWorker();
