6b2f3229c3eb70e2e133d10228bf3c163713b0b4
David Yim <yimd85@gmail.com>
2018-03-19 18:21:22 -0400

    updated favicon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Favicon from 'react-favicon';
 import Home from './home';
 import FaceOff from './faceoff';
 import Overlay from './overlay';
@@ -8,27 +9,30 @@ class App extends Component{
 
 	render() {
 		return(
-			<Switch>
-			      <Route
-			        exact
-			        path="/"
-			        component={Home}
-			      />
+			<div>
+				<Favicon url="https://i.imgur.com/oCRyRm3.png" />
+				<Switch>
+					<Route
+						exact
+						path="/"
+						component={Home}
+					/>
 					<Route
 						exact
 						path="/overlay"
 						component={Overlay}
 					/>
-			      <Route
-			        exact
-			        path="/home"
-			        component={Home}
-			      />
-			      <Route
-			        path="/faceoff"
-			        component={FaceOff}
-			      />
-		    </Switch>
+					<Route
+						exact
+						path="/home"
+						component={Home}
+					/>
+					<Route
+						path="/faceoff"
+						component={FaceOff}
+					/>
+				</Switch>
+			</div>
 		)
 	}
 }
